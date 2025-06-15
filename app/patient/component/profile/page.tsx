d20bf9862402ef2f0bd26bcf5ab1d197675b938c
Rahul kumar <rahulk08372@gmail.com>
2025-06-15 17:33:40 +0530

    final fixing the issues

---
+++ app/patient/component/profile/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const PatientProfilePage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-4">Patient Profile</h1>
+      {/* Add your patient profile content here */}
+    </div>
+  );
+};
+
+export default PatientProfilePage;
